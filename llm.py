0ed3bec6f415e6aa0bcf4c4e363d75d8ccafd234
Daniel Kareti <danielkareti@gmail.com>
2025-11-04 13:27:10 -0800

    Got working llm call; succeptible to hallucinations

--- llm.py
+++ llm.py
@@ -1,3 +1,33 @@
 '''
 This file will prompt the user, call the llm, and respond to the user.
 '''
+#import libraries
+from transformers import pipeline
+
+
+
+def prompt_user(user_prompt: str) -> str:
+    '''
+    args:
+    prompt (str)    : The prompt the user wants to use for the llm.
+    returns:
+    (str)           : This is the response from the user
+    '''
+    get_user_input = input(user_prompt)
+    return get_user_input
+
+def call_model(llm_prompt: str) -> str:
+    '''
+    '''
+
+    pipe = pipeline("text-generation", model="crumb/nano-mistral")
+    result = pipe(llm_prompt)
+    return result[0]['generated_text'] #the generated text
+
+if __name__ == '__main__':
+    string = 'Enter a question: '
+    user_query = prompt_user(string)
+
+    ans = call_model(user_query)
+    print(f'The answer from the llm: {ans}')
+
