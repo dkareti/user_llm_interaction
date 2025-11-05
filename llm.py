094b461610a7f443f40290708fb5bdee92aad906
Daniel Kareti <danielkareti@gmail.com>
2025-11-04 16:40:07 -0800

    added the google flan model, produces better, but still incorrect results

--- llm.py
+++ llm.py
@@ -19,15 +19,20 @@ def prompt_user(user_prompt: str) -> str:
 def call_model(llm_prompt: str) -> str:
     '''
     '''
+    pipe = pipeline("text2text-generation", model="google/flan-t5-base")
+    result = pipe(llm_prompt, max_new_tokens=100)
+    return result[0]["generated_text"]
 
-    pipe = pipeline("text-generation", model="crumb/nano-mistral")
-    result = pipe(llm_prompt)
-    return result[0]['generated_text'] #the generated text
+    '''OPTION 1; YIELDS MANY HALLUCINATIONS'''
+    # pipe = pipeline("text-generation", model="crumb/nano-mistral")
+    # result = pipe(llm_prompt)
+    # return result[0]['generated_text'] #the generated text
 
 if __name__ == '__main__':
     string = 'Enter a question: '
     user_query = prompt_user(string)
 
+    # call_model(user_query)
     ans = call_model(user_query)
     print(f'The answer from the llm: {ans}')
 
